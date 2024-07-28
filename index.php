ba2d06d6b24a73865fbd0518dbeab057078b1639
olavidesdcena22m3 <150347736+Maltesersss@users.noreply.github.com>
2024-07-28 23:32:02 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+        <Head>
+                <title> Online Slumbook </title>
+                <link rel="preconnection" href="https://fonts.gstatic.com"
+                <link href="http://fonts.googleapis.com/css2family=Poppins:wght@300&display=swap" rel="stylesheet">
+                <link rel="stylesheet" type="text/css" href="style.css">
+
+        </Head>
+        <Body>
+            <div class="main">
+                <div class="menu">
+                        <h2 id="logo">LOGO</h2>
+                        <a href="#">Arra Mae L. Abello</a>
+                        <a href="#">Hannah Jane C. Custodio</a>
+                        <a href="#">Elaine Joy R. Fuentes</a>
+                        <a href="#">Nikko A. Loniego</a>
+                        <a href="#">Joseph Bal Justin L. Magpusao</a>
+                </div>
+                <div class="body">
+                            
+                   
+            </div>
+</div>
+        </Body>
+</html>
